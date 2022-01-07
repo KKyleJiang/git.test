516968a801ed3b67c18e00b8e8e2efd480956e6b
Kyle <Kyle@163.com>
2022-01-07 17:10:54 +0800

    IDEA commit1

---
+++ src/main/java/com/kyle/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.kyle.git;
+
+/**
+ * # Description:
+ *
+ * @author KyleJiang
+ * @version #Date: 2022/1/7 16:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
